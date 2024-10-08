3ba953cad62d18578c80c7f52401244bf9cb3434
Maisquasar <romainbou@icloud.com>
2024-10-08 10:40:55 +0200

    [Add] define Imgui Implementation and Math operators in galaxyApi.h

--- GalaxyEngine/include/GalaxyAPI.h
+++ GalaxyEngine/include/GalaxyAPI.h
@@ -2,6 +2,11 @@
 namespace GALAXY {}
 using namespace GALAXY;
 
+#ifndef IMGUI_IMPLEMENTATION // for galaxy maths lib
+	#define IMGUI_IMPLEMENTATION
+	#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
+
 #if defined(_WIN32)
 	#if defined(GALAXY_EXPORTS)
 		#define GALAXY_API __declspec(dllexport)
