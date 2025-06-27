125b3e1636dca5f45ecf79709c169a501e3169d9
Maisquasar <romainbou77@gmail.com>
2025-06-27 16:25:23 +0200

    [Fix?] Linux compilation

--- GalaxyEngine/include/Debug/Log.h
+++ GalaxyEngine/include/Debug/Log.h
@@ -151,4 +151,8 @@ namespace GALAXY::Debug
 #define PrintWarning(x, ...) Debug::Log::Print(__FILENAME__, __LINE__, Debug::LogType::L_WARNING, x, ##__VA_ARGS__)
 #define PrintError(x, ...) Debug::Log::Print(__FILENAME__, __LINE__, Debug::LogType::L_ERROR, x, ##__VA_ARGS__)
 
+#ifdef _WIN32
 #define ASSERT(x) if (!(x)) { PrintError("Assertion failed: %s", #x); __debugbreak(); }
+#else
+#define ASSERT(x) if (!(x)) { PrintError("Assertion failed: %s", #x); abort(); }
+#endif
